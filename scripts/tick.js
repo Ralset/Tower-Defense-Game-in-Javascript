ca3718cb227f831d3c0374758a32713c44b3e633
svuskovic22 <tesla89karate@gmail.com>
2026-01-13 18:58:58 +0100

    Implement Tick class

--- scripts/tick.js
+++ scripts/tick.js
@@ -1,11 +1,36 @@
-PAUSED = false;
-
-let globalTickCount = 0;
-setInterval(() => {
-    if (PAUSED) return;
-    globalTickCount++;
-    // svake sekunde valjda
-    if (globalTickCount % 60 === 0){
-        globalTickCount = 0;
+class Tick {
+    constructor(TPS = 100){
+        this.ticksPerSecond = TPS;
+        this.delay = 1000 / TPS; 
+        this.subscribers = new Set();
+        this.timerId = null;
+        this.tickCount = 0;
     }
-}, 16.67);
+
+    Start(){
+        this.Stop();
+        this.timerId = setInterval(() => {
+            this.tickCount++;
+            this._notify(this.tickCount);
+        }, this.delay);
+    }
+
+    Stop(){
+        if (this.timerId === null) return;
+        clearInterval(this.timerId);
+        this.timerId = null;
+    }
+
+    Subscribe(func, freq){
+        const sub = { func, freq };
+        this.subscribers.add(sub);
+        return () => this.subscribers.delete(sub);
+    }
+
+    _notify(tick){
+        for (const sub of this.subscribers){
+            if(tick % sub.freq === 0)
+                sub.func();
+        }
+    }
+}
